65c61d246cdb50cc32bf24be34ca747202b3db28
Derrick Marques <127461231+Djmarques18@users.noreply.github.com>
2023-10-26 19:17:20 -0400

    Create weekday_name.py

---
+++ weekday_name.py
@@ -0,0 +1,8 @@
+def weekday_name(day_of_week):
+    days = {1: 'sunday', 2: 'monday', 3: 'tuesday', 4: 'wednsday', 5: 'thursday', 6: 'friday', 7: 'saturday'}
+    if day_of_week >= 1 and day_of_week <= 7:
+     if day_of_week in days:
+        day_name = days[day_of_week]
+        print(day_name)
+    else:
+        print("IMVALID DAY NUMBER!!")
